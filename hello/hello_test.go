17e293b34aec6f39ce8774a6ad8a323f4f413f39
klkelley <kelley.15@gmail.com>
2020-12-10 17:02:36 -0600

    Add more languages

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -1,4 +1,4 @@
-package main 
+package main
 
 import "testing"
 
@@ -10,16 +10,27 @@ func TestHello(t *testing.T) {
     }
   }
 
-
   t.Run("saying hello to people", func(t *testing.T) {
-    got := Hello("Kara")
+    got := Hello("Kara", "English")
     want := "Hello, Kara"
     assertCorrectMessage(t, got, want)
   })
 
   t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-    got := Hello("")
+    got := Hello("", "")
     want := "Hello, World"
+    assertCorrectMessage(t, got, want)
+  })
+
+  t.Run("in Spanish", func(t *testing.T) {
+    got := Hello("Elodie", "Spanish")
+    want := "Hola, Elodie"
+    assertCorrectMessage(t, got, want)
+  })
+
+  t.Run("in French", func(t *testing.T) {
+    got := Hello("Kelley", "French")
+    want := "Bonjour, Kelley"
     assertCorrectMessage(t, got, want)
   })
 }
